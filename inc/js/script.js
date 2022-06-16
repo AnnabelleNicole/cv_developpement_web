46e6867c4ea21f899dd8e905f134086c4276be50
Annabelle Nicole <annabelle22003@hotmail.com>
2022-06-16 16:38:05 -0400

    commit

---
+++ inc/js/script.js
@@ -0,0 +1,34 @@
+function contraintes() {
+  if (document.getElementById("contraintes").style.display != "block") {
+    document.getElementById("contraintes").style.display = "block";
+  }
+  else {
+    document.getElementById("contraintes").style.display = "none"
+  }
+}
+
+function enSavoirPlus() {
+  if (document.getElementById("en-savoir-plus").style.display != "block") {
+    document.getElementById("en-savoir-plus").style.display = "block";
+  }
+  else {
+    document.getElementById("en-savoir-plus").style.display = "none"
+  }
+}
+
+function analyse() {
+  if (document.getElementById("analyse").style.display != "block") {
+    document.getElementById("analyse").style.display = "block";
+  }
+  else {
+    document.getElementById("analyse").style.display = "none"
+  }
+}
+function bd() {
+  if (document.getElementById("bd").style.display != "block") {
+    document.getElementById("bd").style.display = "block";
+  }
+  else {
+    document.getElementById("bd").style.display = "none"
+  }
+}
